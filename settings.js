73e1112832e56abbdbc6985a52a57bd9286b055d
virakun <93888991+virakun@users.noreply.github.com>
2022-02-19 07:41:52 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,19 +18,19 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "BOT VVIP"
+global.botname = "VVIP BOT"
 global.ownername= "DOI"
-global.myweb ="https://api-inabot.herokuapp.com/"
-global.youtube = "https://youtube.com/c/Ina"
-global.github = "https://inaofc.github.io/"
+global.myweb ="https://api-doibot.herokuapp.com/"
+global.youtube = "https://youtube.com/c/doi"
+global.github = "https://doi.github.io/"
 global.ownernomer = "6281283408836"
-global.ownernomerr = "6281283408836"
+global.ownernomerr = "+6281283408836"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6281283408836"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© DOI' //sticker wm
-global.author = 'BOT VVIP' //sticker wm
+global.packname = '© VVIP BOT' //sticker wm
+global.author = 'DOI' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
